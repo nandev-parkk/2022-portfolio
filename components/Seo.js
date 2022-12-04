cb2876497f7cb11ab40e66c6351e0a80126be6a7
nandev <nandev.parkk@gmail.com>
2022-12-05 01:58:17 +0900

    seo meta tag 수정

--- components/Seo.js
+++ components/Seo.js
@@ -14,14 +14,22 @@ export default function Seo({ title }) {
         content="width=device-width, initial-scale=1, user-scalable=no, maximum-scale=1"
       />
       <title>{title}</title>
-      <meta property="og:url" content="https://*.html" />
-      <meta property="og:title" content="..." />
-      <meta property="og:description" content="..." />
-      <meta property="og:image" content="https://*.jpg" />
+      <meta property="og:url" content={domain} />
+      <meta property="og:title" content={title} />
+      <meta
+        property="og:description"
+        content="프론트엔드 개발자 박상훈의 포트폴리오"
+      />
+      <meta property="og:image" content="" />
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:title" content="..." />
-      <meta name="twitter:description" content="..." />
-      <meta name="twitter:image" content="https://*.jpg" />
+      <meta name="twitter:title" content={title} />
+      <meta
+        name="twitter:description"
+        content="프론트엔드 개발자 박상훈의 포트폴리오"
+      />
+      <meta name="twitter:image" content="" />
     </Head>
   );
 }
+
+const domain = process.env.NEXT_PUBLIC_DOMAIN;
