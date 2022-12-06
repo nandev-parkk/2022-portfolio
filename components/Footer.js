535cfbc03d4759a49ddc37f5fe2c2635c2b2d487
nandev <nandev.parkk@gmail.com>
2022-12-06 17:32:43 +0900

    edit footer font-size

--- components/Footer.js
+++ components/Footer.js
@@ -5,10 +5,15 @@ export default function Footer() {
   return <footer css={footer}>&copy; 2022. nandev All rights reserved.</footer>;
 }
 
-const color = { commonStyles };
+const { color, font } = commonStyles;
 
 const footer = css`
   color: ${color.white};
   text-align: center;
   padding: 40px 0;
+
+  @media screen and (max-width: 425px) {
+    font-size: ${font.size.sm};
+    padding: 20px 0;
+  }
 `;
