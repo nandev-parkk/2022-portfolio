f07bfe6006aa2bd3251f733a9308395bf76cd431
nandev <nandev.parkk@gmail.com>
2022-12-07 21:06:35 +0900

    add renderer func

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,23 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { renderStatic } from "utils/renderer";
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const { css, ids } = await renderStatic(initialProps.html);
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          <style
+            data-emotion={`css ${ids.join(" ")}`}
+            dangerouslySetInnerHTML={{ __html: css }}
+          />
+        </>
+      ),
+    };
+  }
   render() {
     return (
       <Html>
