3881fb3689abef9098541f37d2ea5380ba7f5860
nandev <nandev.parkk@gmail.com>
2022-12-08 02:37:18 +0900

    edit global css

--- styles/global.js
+++ styles/global.js
@@ -3,7 +3,7 @@ import commonStyles from "./common";
 
 const { color, transition } = commonStyles;
 
-const global = () => css`
+const global = css`
   @import url("https://fonts.googleapis.com/css2?family=Rajdhani:wght@300;400;500;600;700&display=swap");
   @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap");
   @import url("https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap");
